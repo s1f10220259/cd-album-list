0791e53389da77d935bf463f80156591d7650edd
syouki sugiyama <s1F102202594@iniad.org>
2024-10-31 18:00:20 +0900

    修正

--- album/views.py
+++ album/views.py
@@ -3,8 +3,9 @@ from album.models import Albums
 
 # Create your views here.
 def index(request):
+    albums = Albums.objects.all()
     #albums = Albums.objects.select_related('artist').all()
-    albums = Albums.objects.prefetch_related('artist').all()
+    #albums = Albums.objects.prefetch_related('artist').all()
     return render(request, 'album/index.html', context=locals())
 
 def detail(request, album_id):
